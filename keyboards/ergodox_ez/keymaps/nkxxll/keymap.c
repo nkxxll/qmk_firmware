73ccd4c69ff811a2206248ecf84383794be03257
nkirschall <niki.nice1203@gmail.com>
2023-09-30 23:00:22 +0200

    feat: (){}[] are better now also 123456789 are better now

--- keyboards/ergodox_ez/keymaps/nkxxll/keymap.c
+++ keyboards/ergodox_ez/keymaps/nkxxll/keymap.c
@@ -36,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_WWW_FORWARD,                                 KC_WWW_BACK,    KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_BSLS,      KC_QUOTE,       KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_PIPE,        KC_LBRC,    KC_LCBR,        KC_LPRN,        KC_EQUAL,                                                                       KC_MINUS,       KC_RPRN,        KC_RCBR,        KC_RBRC,    KC_DQUO,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_BRIGHTNESS_UP,                                KC_BRIGHTNESS_DOWN,KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_WWW_FORWARD,                                 KC_WWW_BACK,    KC_CIRC,        KC_1,           KC_2,           KC_3,           KC_QUOTE,       KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_ASTR,        KC_LCBR,        KC_LPRN,        KC_RPRN,        KC_RCBR,                                                                        KC_AMPR,        KC_4,           KC_5,           KC_6,           KC_DQUO,        KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_BSLS,      KC_PIPE,        KC_LBRC,    KC_RBRC,    KC_EQUAL,       KC_BRIGHTNESS_UP,                                KC_BRIGHTNESS_DOWN,KC_MINUS,       KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
