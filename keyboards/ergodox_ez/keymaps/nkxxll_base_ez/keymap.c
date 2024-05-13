fd0e22597246128819bf04edf646d5fdb8417e14
nkxxll <niki.nice1203@gmail.com>
2024-05-13 22:15:46 +0200

    this are some nice changes to the arrow keys

--- keyboards/ergodox_ez/keymaps/nkxxll_base_ez/keymap.c
+++ keyboards/ergodox_ez/keymaps/nkxxll_base_ez/keymap.c
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_MINS, KC_Q, KC_W, KC_E, KC_R, KC_T, TT(SYMB), TT(MDIA), KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQL,
             KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
             KC_UNDS, KC_Z, KC_X, KC_C, KC_V, KC_B, ALL_T(KC_NO), MEH_T(KC_NO), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
-            KC_LALT, KC_LEFT, KC_RGHT,  KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_DOWN, KC_UP, KC_LGUI,
+            KC_LALT, KC_DEL, KC_INS,  KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_PGDN, KC_PGUP, KC_LGUI,
             LCTL_T(KC_ESC), KC_LGUI, KC_LALT, RCTL_T(KC_GRV),
-            KC_PGUP, KC_PGDN,
-            KC_LSFT, KC_ENTER, KC_DEL, KC_INS, KC_BSPC, KC_SPACE),
+            KC_LEFT, KC_UP,
+            KC_LSFT, KC_ENTER, KC_RGHT, KC_DOWN, KC_BSPC, KC_SPACE),
     // this is the two layer approach I want to try the one layer approach again
     // KC_LSFT, KC_ENTER, KC_DEL, KC_INS, KC_BSPC, LT(SYMB, KC_SPACE)),
 
